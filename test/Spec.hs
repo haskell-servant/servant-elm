78a7eb7759c1901834bdec0cdfa7f66eede3be3a
Matt Bray <mattjbray@gmail.com>
2015-11-18 21:31:32 +0000

    tests!

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+
+import Test.Hspec
+
+import Data.Proxy (Proxy(Proxy))
+import Servant.API
+import Servant.Elm
+
+
+type TestApi = "one" :> Get '[JSON] Int
+          :<|> "two" :> ReqBody '[JSON] String :> Post '[JSON] (Maybe Int)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $
+  describe "encoding a simple api" $
+    it "does it" $ do
+      getOneSource <- readFile "test/getOneSource.txt"
+      postTwoSource <- readFile "test/postTwoSource.txt"
+      map (++ "\n") (generateElmForAPI (Proxy :: Proxy TestApi)) `shouldBe` [getOneSource, postTwoSource]
