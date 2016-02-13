b462ea53868386cee7986772647237d40dbd4c5f
Matt Bray <mattjbray@gmail.com>
2016-02-13 22:57:17 +0000

    Update example output

--- examples/books/elm/Generated/BooksApi.elm
+++ examples/books/elm/Generated/BooksApi.elm
@@ -25,40 +25,58 @@ encodeBook x =
 
 postBooks : Book -> Task.Task Http.Error (Book)
 postBooks body =
-  let request =
-        { verb = "POST"
-        , headers = [("Content-Type", "application/json")]
-        , url = "http://localhost:8000"
-             ++ "/" ++ "books"
-        , body = Http.string (Json.Encode.encode 0 (encodeBook body))
-        }
-  in  Http.fromJson
-        decodeBook
-        (Http.send Http.defaultSettings request)
+  let
+    request =
+      { verb =
+          "POST"
+      , headers =
+          [("Content-Type", "application/json")]
+      , url =
+          "http://localhost:8000"
+          ++ "/" ++ "books"
+      , body =
+          Http.string (Json.Encode.encode 0 (encodeBook body))
+      }
+  in
+    Http.fromJson
+      decodeBook
+      (Http.send Http.defaultSettings request)
 
 getBooks : Task.Task Http.Error (List (Book))
 getBooks =
-  let request =
-        { verb = "GET"
-        , headers = [("Content-Type", "application/json")]
-        , url = "http://localhost:8000"
-             ++ "/" ++ "books"
-        , body = Http.empty
-        }
-  in  Http.fromJson
-        (Json.Decode.list decodeBook)
-        (Http.send Http.defaultSettings request)
+  let
+    request =
+      { verb =
+          "GET"
+      , headers =
+          [("Content-Type", "application/json")]
+      , url =
+          "http://localhost:8000"
+          ++ "/" ++ "books"
+      , body =
+          Http.empty
+      }
+  in
+    Http.fromJson
+      (Json.Decode.list decodeBook)
+      (Http.send Http.defaultSettings request)
 
 getBooksBy : Int -> Task.Task Http.Error (Book)
 getBooksBy bookId =
-  let request =
-        { verb = "GET"
-        , headers = [("Content-Type", "application/json")]
-        , url = "http://localhost:8000"
-             ++ "/" ++ "books"
-             ++ "/" ++ (bookId |> toString |> Http.uriEncode)
-        , body = Http.empty
-        }
-  in  Http.fromJson
-        decodeBook
-        (Http.send Http.defaultSettings request)
+  let
+    request =
+      { verb =
+          "GET"
+      , headers =
+          [("Content-Type", "application/json")]
+      , url =
+          "http://localhost:8000"
+          ++ "/" ++ "books"
+          ++ "/" ++ (bookId |> toString |> Http.uriEncode)
+      , body =
+          Http.empty
+      }
+  in
+    Http.fromJson
+      decodeBook
+      (Http.send Http.defaultSettings request)
