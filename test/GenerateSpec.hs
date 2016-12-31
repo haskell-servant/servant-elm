9c0fa2ab0c84c4ba7f5662c3d2faeb6e76bb8617
Matt Bray <mattjbray@gmail.com>
2016-12-31 17:02:19 +0000

    Allow passing the base URL dynamically in Elm.

--- test/GenerateSpec.hs
+++ test/GenerateSpec.hs
@@ -12,11 +12,12 @@ import           Data.Monoid               ((<>))
 import           Data.Text                 (Text)
 import qualified Data.Text                 as T
 import qualified Data.Text.IO              as T
+import           Servant.API               ((:>), Get, JSON)
 import           Servant.Elm
 import           Test.Hspec                (Spec, describe, hspec, it)
 import           Test.HUnit                (Assertion, assertBool)
 
-import Common (testApi)
+import           Common                    (testApi)
 
 
 main :: IO ()
@@ -64,6 +65,25 @@ spec = do
                               "import Http\n" <>
                               "import Json.Decode exposing (..)\n\n\n")]
                   let generated = map (<> "\n") (generateElmForAPI testApi)
+                  generated `itemsShouldBe` expected
+           it "with dynamic URLs" $
+               do expected <-
+                      mapM
+                          (\(fpath,header) -> do
+                               source <- T.readFile fpath
+                               return (fpath, header, source))
+                          [ ( "test/elm-sources/getOneWithDynamicUrlSource.elm"
+                            , "module GetOneWithDynamicUrlSource exposing (..)\n\n" <>
+                              "import Http\n" <>
+                              "import Json.Decode exposing (..)\n\n\n")]
+                  let generated =
+                          map
+                              (<> "\n")
+                              (generateElmForAPIWith
+                                   (defElmOptions
+                                    { urlPrefix = Dynamic
+                                    })
+                                   (Proxy :: Proxy ("one" :> Get '[JSON] Int)))
                   generated `itemsShouldBe` expected
 
 itemsShouldBe :: [Text] -> [(String, Text, Text)] -> IO ()
