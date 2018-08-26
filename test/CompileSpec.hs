bc09fc3c8ed4223c225749af3976d37dac3f4d92
Matt Bray <mattjbray@gmail.com>
2018-08-26 12:35:28 +0100

    Fix integration test runner for 0.19

--- test/CompileSpec.hs
+++ test/CompileSpec.hs
@@ -6,8 +6,6 @@ module Main where
 import           Test.Hspec
 import           Test.Mockery.Directory
 
-import           Control.Exception
-import           Control.Monad                (when)
 import           Data.String.Interpolate
 import           Data.String.Interpolate.Util
 import qualified Data.Text                    as T
@@ -17,11 +15,6 @@ import           Elm                          (toElmDecoderSource,
                                                toElmTypeSource)
 import           Servant.API                  (NoContent)
 import           Servant.Elm
-import           System.Directory             (canonicalizePath,
-                                               createDirectoryIfMissing,
-                                               doesDirectoryExist,
-                                               getCurrentDirectory, removeFile,
-                                               setCurrentDirectory)
 import           System.Process
 
 import Common (Book, testApi)
@@ -34,7 +27,30 @@ spec :: Test.Hspec.Spec
 spec = do
   describe "generateElmForAPI" $ do
     it "creates compilable javascript" $ do
-      inTempElmDir $ do
+      inTempDirectory $ do
+        writeFile "elm.json" $ unindent $ [i|
+          {
+              "type": "application",
+              "source-directories": [
+                  "."
+              ],
+              "elm-version": "0.19.0",
+              "dependencies": {
+                  "direct": {
+                      "elm/core": "1.0.0",
+                      "elm/json": "1.0.0",
+                      "elm-community/json-extra": "3.0.0",
+                      "elm/http": "1.0.0",
+                      "NoRedInk/elm-json-decode-pipeline": "1.0.0"
+                  },
+                  "indirect": {}
+              },
+              "test-dependencies": {
+                  "direct": {},
+                  "indirect": {}
+              }
+          }
+        |]
         let generated =
               T.intercalate "\n\n" $
                 defElmImports :
@@ -45,58 +61,4 @@ spec = do
                 ] ++
                 generateElmForAPI testApi
         T.writeFile "Api.elm" generated
-        callCommand "elm-make Api.elm --output api.js"
-
-inTempElmDir :: IO a -> IO a
-inTempElmDir action = do
-  cacheExists <- doesDirectoryExist "_test-cache"
-  when (not cacheExists)
-    createCache
-  cacheDir <- canonicalizePath "_test-cache"
-  inTempDirectory $ do
-    callCommand ("cp -r " ++ cacheDir ++ "/* .")
-    action
-
-createCache :: IO ()
-createCache = do
-  createDirectoryIfMissing True "_test-cache"
-  withCurrentDirectory "_test-cache" $ do
-    writeFile "elm-package.json" $ unindent $ [i|
-      {
-          "version": "1.0.0",
-          "summary": "helpful summary of your project, less than 80 characters",
-          "repository": "https://github.com/user/project.git",
-          "license": "BSD3",
-          "source-directories": [
-              "."
-          ],
-          "exposed-modules": [],
-          "dependencies": {
-              "elm-lang/core": "5.0.0 <= v < 6.0.0",
-              "elm-community/json-extra": "2.0.0 <= v < 3.0.0",
-              "elm-lang/http": "1.0.0 <= v < 2.0.0",
-              "NoRedInk/elm-decode-pipeline": "3.0.0 <= v < 4.0.0",
-              "NoRedInk/string-conversions": "1.0.0 <= v < 2.0.0"
-          },
-          "elm-version": "0.18.0 <= v < 0.19.0"
-      }
-    |]
-    callCommand "elm-package install --yes"
-    compileDependencies
-
-compileDependencies :: IO ()
-compileDependencies = do
-  writeFile "Main.elm" "foo = 42"
-  callCommand "elm-make Main.elm --output main.js"
-  removeFile "Main.elm"
-  removeFile "main.js"
-
-withCurrentDirectory :: FilePath -> IO a -> IO a
-withCurrentDirectory dir action =
-  bracket enter recover (const action)
-  where
-    enter = do
-      original <- getCurrentDirectory
-      setCurrentDirectory dir
-      return original
-    recover = setCurrentDirectory
+        callCommand "elm make Api.elm --output api.js"
